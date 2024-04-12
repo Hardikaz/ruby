9b372bfb011d02315205b087a105c105f4c97f26
rt4914 <rajattalesra4914@gmail.com>
2024-04-13 01:24:12 +0530

    complete ruby programming course 2024

---
+++ 01_print.rb
@@ -0,0 +1,21 @@
+# print vs puts
+
+# print "Hello World"
+# puts 'Like, share and subscribe'
+
+# # Predict output of these examples
+# # Example 1
+# puts "Hello"
+# print "World"
+
+# Example 2
+# print "Hello"
+# puts "World"
+
+# # Example 3
+# print "Hello"
+# print "World"
+
+# # Example 4
+# puts "Hello"
+# puts "World"
---
+++ README.md
@@ -0,0 +1,3 @@
+# Ruby Programming Crash Course 2024
+
+Checkout my youtube video to understand this completely: https://youtu.be/w9yNsTAp-Mg
